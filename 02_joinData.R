78a8f71a06b2efedeff3f40ddd2f5151f69e84eb
pacheco-andrea <pacheco.gracia@gmail.com>
2024-04-17 22:37:46 +0200

    cleaned up tests, made area km2, final check of numbers

--- 02_joinData.R
+++ 02_joinData.R
@@ -11,7 +11,7 @@ library(dplyr)
 library(ggplot2)
 
 #directory
-wdmain <- "N:/eslu/work/CLEVER/EUDR_soyMoratorium/"
+wdmain <- "N:/eslu/priv/pacheco/EUDR_blindspot/"
 setwd(wdmain)
 
 # get the polygon intersections ----
@@ -27,39 +27,28 @@ lapply(data, summary)
 # next, put all data together
 unlist(lapply(data, nrow))
 data <- do.call(rbind, data)
-data$area <- as.numeric((st_area(data))/10000) # convert from m^2 to ha to match the other vars
+data$area <- as.numeric((st_area(data))/1000000) # convert from m^2 to km2
 summary(data)
 data$year <- as.factor(data$year)
-
-
 # NOTE: there are duplicated fids in this data
 # this is because although the deforestation polygons were unique obs, 
 # they can overlap with multiple properties. the resulting polygons are all those areas which overlapped. 
 
-# 
-length(unique(data$fid)) # the original number is 552752
-# this is 69,262 observations less than the INPE deforestation - but 
-test <- data[which(duplicated(data$fid)),]
-test[which(test$fid == 239903 ),]
-
-plot(deforPoly[which(deforPoly$fid == 239903 ),]$geometry)
-plot(test[which(test$fid == 239903 ),]$geometry, add = T)
-plot(test[which(test$fid == 239903 ),"tipo"])
 
 # from this, I can graph: ----
 
 # how much deforestation happened illegally (in properties with deficit)?
 # how much deforestation happened legally (in properties with surplus)?
 
-# create legality variable
-# we need to set this as a starting point because there are deforestation polygons that have neither surplus nor deficit - in theory, legal
+# create legality variable ----
+# we need to set "legal" as a starting point because 
+# there are deforestation polygons that overlapped with properties that had neither surplus nor deficit
+# in theory, this deforestation is legal/compliant
 data$legality <- "legal" 
-# identify cases where there is veg surplus
-# would also be legal, so the part below is redundant
-# data[which(data$rl_ativo > 0),]$legality <- "legal"
+# this already includes cases where there is veg surplus (rl_ativo_)
 
-# now identify the observations with deforestation where there was a deficit for either RL or APPs
-# note this could overwrite the places where there was rl ativo (surplus), because app deficit is still be potentially illegal
+# now identify the observations with deforestation where there was a deficit for either RL OR APPs
+# all of these are potentially illegal
 nrow(data[which(data$rl_def < 0 | data$app_def < 0),])
 data[which(data$rl_def < 0 | data$app_def < 0),]$legality <- "illegal"
 data$legality <- as.factor(data$legality)
@@ -75,7 +64,7 @@ mycols <- c("#cc4c02", "#fee391")
 
 # plot legal and illegal deforestation
 deforestation <- ggplot(data) +
-  geom_col(aes(year, (area/100), fill = legality)) +
+  geom_col(aes(year, (area), fill = legality)) +
   scale_fill_manual(values = mycols) +
   labs(y = bquote("Area km"^2), fill = "Deforestation") +
   scale_y_continuous(labels = function(x) format(x, scientific = FALSE)) +
@@ -88,10 +77,8 @@ deforestation <- ggplot(data) +
         legend.position = c(0.77, 0.87))
 deforestation
 
-# this is freaking me out so i should just compare with the original INPE data
-
 setwd(paste0(wdmain, "output/"))
-png("illegalDeforestationPlot_2.png", units = "cm", width = 16, height = 6, res = 300)
+png("illegalDeforestationPlot.png", units = "cm", width = 16, height = 6, res = 300)
 deforestation
 dev.off()
 # write out data too
